1e18593bea1e7163d76ccdce311c19873c515527
Aabondd <aabondd@gmail.com>
2020-09-30 01:48:17 +0300

    Updates

---
+++ precache-manifest.7d03e44d548ce56f32da44811f6216df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2603c33d530e47e687b9894d04f9a36",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "a79532d1ed433f577cf8",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.00078bec.chunk.css"
+  },
+  {
+    "revision": "8ada5ca0fb3267963dd4",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.4528e366.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.4528e366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79532d1ed433f577cf8",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.ed52bb32.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
